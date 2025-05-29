98023b8053c57fecfb53bcff7d5a7f12a714943c
Alejandro Cerezo <alce65@hotmail.es>
2025-05-29 14:58:58 +0200

    Update Products page

--- projects/demo3/src/features/products/products.tsx
+++ projects/demo3/src/features/products/products.tsx
@@ -1,17 +1,33 @@
 import { Link } from 'react-router';
 import './products.css';
+import type { Product } from './types/product';
+
+import { use, useEffect, useState } from 'react';
+import { AppContext } from '@context/context';
 
 export const Products: React.FC = () => {
+  
+  const [products, setProducts] = useState<Product[]>([]);
+  const {productsRepo: repo} = use(AppContext)
+
+  useEffect(() => {
+    repo.getProducts().then((data) => {
+        setProducts(data);
+        }).catch((error) => {
+        console.error('Error fetching products:', error);
+    });
+  }, [repo]);
+
   return (
     <section className="products">
       <h2>Welcome to the Demo Project Products</h2>
-      <p>
-        This project demonstrates the use of TypeScript, React, Vite, and
-        Vitest.
-      </p>
-      <p>
-        <Link to="/product/1">View Product 1</Link>
-      </p>
+      <ul className="products-list">
+        {products.map((item) => (
+          <li className="product-item" key={item.id}>
+            <Link to={'/product/' + item.id}>{item.name}</Link>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 };
